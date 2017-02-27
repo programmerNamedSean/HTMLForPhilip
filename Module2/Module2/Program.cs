f7b806fa0deb5ad1b11ef769c22d47aa33292f6c
programmerNamedSean <sesulli@siue.edu>
2017-02-27 08:13:14 -0600

    Second commit

---
+++ Module2/Module2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //String storing an odd row of the chess board represented in Xs and Os 
+            string oddRow = "XOXOXOXO";
+            //String storing an even row of the chess board represented in Xs and Os 
+            string evenRow = "OXOXOXOX";
+            //A for loop which constructs the board
+            for (int i = 0; i < 7; i++)
+            {
+                //Checks to see if this is the first row.
+                if(i ==0)
+                {
+                    //Prints out the odd row String representation.
+                    Console.WriteLine(oddRow);
+                }
+                //Checks to see if this is an even row.               
+                if(i %2==0)
+                {
+                    //Prints out the even row String representation.
+                    Console.WriteLine(evenRow);
+                }else//Assumes the rest are odd rows.
+                {
+                    //Prints out the odd row String representation.
+                    Console.WriteLine(oddRow);
+                }
+                //end of decision tree
+            }//end of for loop
+        }//end of main
+    }//end of class
+}//end of namespace
---
+++ Module4/Module4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            student[] studentsInClass = new student[5];
+            for(int i = 0; i <5; i++)
+            {
+                studentsInClass[i] = new student("John","Doe","May 14, 1987");
+            }
+            foreach(student s in studentsInClass)
+            {
+                Console.WriteLine("{0} {1} was born on: {2}", s.firstName, s.lastName, s.birthday);
+            }
+        }
+        struct student
+        {
+           public string firstName, lastName, birthday;
+           public student(string fName, string lName, string birthday)
+            {
+                this.firstName = fName;
+                this.lastName = lName;
+                this.birthday = birthday;
+            }
+        }
+        struct teacher
+        {
+            public string firstName, lastName, birthday;
+            public teacher(string firstName, string lastName, string birthday)
+            {
+                this.firstName = firstName;
+                this.lastName = lastName;
+                this.birthday = birthday;
+            }
+        }
+        struct UProgram
+        {
+            public string nameOfTheProgram,  departmentHead,  degrees;
+            public UProgram(string nameOfTheProgram, string departmentHead, string degrees)
+            {
+                this.nameOfTheProgram = nameOfTheProgram;
+                this.departmentHead = departmentHead;
+                this.degrees = degrees;
+            }
+        }
+        struct Course
+        {
+            public string courseName, credits, duration;
+            public Course(string courseName, string credits, string duration)
+            {
+                this.courseName = courseName;
+                this.credits = credits;
+                this.duration = duration;
+            }
+        }
+    }
+}
